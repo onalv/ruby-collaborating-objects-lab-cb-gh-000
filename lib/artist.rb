88f1a2653db8f5a888f0d5354dc2a8c8bbdd38d6
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-22 20:54:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@song_count += 1
+    # @@song_count += 1
     song.artist = self
   end
 
@@ -31,13 +31,64 @@ class Artist
   #   @@song_count
   # end
 
-  def self.find_or_create_by_name(artist_name)
-    if !self.all.include?(artist_name)
-      self.new(artist_name)
-      save
-    else
-      artist_name
+    def self.create(name)
+      artist = self.new(name)
+      artist.name = name
+      artist
     end
-  end
+  
+    def self.find(name)
+      self.all.detect { |artist| artist.name == name }
+    end
+  
+    def self.find_or_create_by_name(name)
+      self.find(name) || self.create(name)
+    end
 
 end
+# 
+# class Artist
+#   attr_accessor :name, :songs
+# 
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     save
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.create(name)
+#     artist = self.new(name)
+#     artist.name = name
+#     artist
+#   end
+# 
+#   def self.find(name)
+#     self.all.detect { |artist| artist.name == name }
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.find(name) || self.create(name)
+#   end
+# 
+#   def add_song(song)
+#     @songs << song
+#   end
+# 
+#   def print_songs
+#     songs.each { |song| puts song.name }
+#   end
+# 
+#   def save
+#     @@all << self
+#   end
+# 
+#   def songs
+#     @songs
+#   end
+# end
