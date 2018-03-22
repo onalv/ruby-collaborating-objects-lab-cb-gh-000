5a1f380b665ca8b762d21a71c9e3bfd6b0367035
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-22 20:12:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,11 @@
 class Artist
   attr_accessor :name, :songs
   @@song_count = 0
-  # @@all = []
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all = []
   end
 
   def add_song(song)
