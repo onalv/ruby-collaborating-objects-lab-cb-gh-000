cc70f9b0d28734f4233b75f7ba5324a3c6ecec3b
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-22 20:50:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,18 +15,18 @@ class Artist
   def save
     self.class.all << self
   end
-  # 
+  #
   # def add_song(song)
   #   @songs << song
   #   @@song_count += 1
   #   song.artist = self
   # end
-  # 
+  #
   # def add_song_by_name(name_song)
   #   new_song = Song.new(name_song)
   #   add_song(new_song)
   # end
-  # 
+  #
   # def self.song_count
   #   @@song_count
   # end
